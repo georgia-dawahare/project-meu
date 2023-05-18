46a6b48f971cf2c87a7236658b525484d61913cf
Soo <parksk971031@gmail.com>
2023-05-17 21:11:26 -0400

    done

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { LogBox } from "react-native";
+import MainTabBar from './navigation/main_tab_bar';
+
+// disable really annoying in app warnings
+LogBox.ignoreAllLogs();
+
+const App = (props) => {
+  return <MainTabBar />;
+};
+
+
+export default App;
