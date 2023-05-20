438d9ab7f89f36312f9cb3e7b964bc69ba33bb42
Soo <parksk971031@gmail.com>
2023-05-20 18:49:20 -0400

    signinPage complete

--- screens/registration/SigninPage.js
+++ screens/registration/SigninPage.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { LogBox, SafeAreaView } from 'react-native';
+import { LogBox, SafeAreaView, StyleSheet, Text, Image} from 'react-native';
 import Button from '../../components/Button'
 import Registration_Input from '../../components/Registration_Input'
 
@@ -10,7 +10,11 @@ const SigninPage = (props) => {
 
   return(
     <SafeAreaView>
-      {/* <CustomButton onPress={handleButtonPress} title="Press Me" /> */}
+      <Image
+        source={require('../../assets/signup-vector.png')}
+        style={styles.SignupVector}
+      />
+      <Text style={styles.WelcomeMeU}>Welcome to{"\n"}MeU</Text>
       <Button title="Sign Up"/>
       <Registration_Input 
       placeholder="Enter your email"
@@ -18,15 +22,41 @@ const SigninPage = (props) => {
       <Registration_Input 
       placeholder="Enter your Password"
       top={413}/>
-      <Text>Forgot Password?</Text>
+      <Text style={styles.ForgotPassword}>Forgot Password?</Text>
     </SafeAreaView>
   )
 };
 
 const styles = StyleSheet.create({
-  Text: {
+  WelcomeMeU:{
+    position:'absolute',
+    top:60,
+    left:27,
+    color: '#ffffff',
+    fontFamily: 'SFProDisplay-Bold',
+    fontSize: 32,
+    weight:400,
+    letterSpacing:1,
+    lineSpacing:34,
+    alignSelf:'center',
+  },
+  SignupVector: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    resizeMode: 'cover',
+  },
+  ForgotPassword: {
+    position:'absolute',
+    top:597,
+    left:136,
+    color: 'rgba(230, 43, 133, 1)',
+    fontFamily: 'SFProDisplay-Regular',
     fontSize: 14,
-    color: '#828282',
+    weight:400,
+    letterSpacing:1,
     alignSelf:'center',
   },
 });
