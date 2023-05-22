25d55ed0c495f8663c6c920499c21cb9208d9d3f
georgia-dawahare <georgia.g.dawahare.23@dartmouth.edu>
2023-05-22 15:18:38 -0400

    cleaned up files

--- project-meu/App.js
+++ project-meu/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-// import HomePage from './src/screens/home/HomePage';
-// import CheckinPage from './src/screens/checkin/CheckinPage';
-// import PenguinsPage from './src/screens/penguins/PenguinsPage';
 import {
   HomeScreenNavigator,
   CheckinScreenNavigator,
@@ -16,13 +13,6 @@ const Tab = createBottomTabNavigator();
 function App() {
   return (
     <NavigationContainer>
-      {/* <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Home" component={HomePage} />
-        <Stack.Screen name="Checkin" component={CheckinPage} />
-        <Stack.Screen name="CheckinSubmit" component={CheckinSubmit} />
-        <Stack.Screen name="CheckinHistory" component={CheckinHistory} />
-        <Stack.Screen name="Penguins" component={PenguinsPage} />
-      </Stack.Navigator> */}
       <Tab.Navigator
         screenOptions={{
           headerShown: false,
