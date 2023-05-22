519817b4923d607294aa70ca9169d72a88d6d118
georgia-dawahare <georgia.g.dawahare.23@dartmouth.edu>
2023-05-21 23:19:39 -0400

    added font loading example

--- project-meu/src/screens/home/HomePage.js
+++ project-meu/src/screens/home/HomePage.js
@@ -1,24 +1,49 @@
 /* eslint-disable global-require */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   SafeAreaView,
+  Text,
+  StyleSheet,
 } from 'react-native';
+import * as Font from 'expo-font';
 import SignUpGraphic from '../../components/SignUpGraphic';
 
 function HomePage() {
+  // Example font loading
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  useEffect(() => {
+    async function loadFont() {
+      await Font.loadAsync({
+        'SF-Pro-Display-Bold': require('../../../assets/fonts/SF-Pro-Display-Bold.otf'),
+        'SF-Pro-Display-Semibold': require('../../../assets/fonts/SF-Pro-Display-Semibold.otf'),
+
+      });
+
+      setFontLoaded(true);
+    }
+
+    loadFont();
+  }, []);
+
+  if (!fontLoaded) {
+    return <Text>Loading...</Text>;
+  }
+
   return (
     <SafeAreaView>
       <SignUpGraphic />
+      <Text style={styles.signInText}>hello</Text>
     </SafeAreaView>
   );
 }
 
 // Example StyleSheet
-// const styles = StyleSheet.create({
-//   signInText: {
-//     fontFamily: 'SFProDisplay-Semibold',
-//     color: 'rgba(230, 43, 133, 1)',
-//   },
-// });
+const styles = StyleSheet.create({
+  signInText: {
+    fontFamily: 'SF-Pro-Display-Bold',
+    color: 'rgba(230, 43, 133, 1)',
+  },
+});
 
 export default HomePage;
