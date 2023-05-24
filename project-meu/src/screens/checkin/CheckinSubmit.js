adf810f777a1ac8e32cea25e2eabe5e7f0cff9e7
Kaylie Sampson <kksamps2@gmail.com>
2023-05-24 00:15:37 -0400

    added response connection to backend

--- project-meu/src/screens/checkin/CheckinSubmit.js
+++ project-meu/src/screens/checkin/CheckinSubmit.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Text,
   SafeAreaView,
@@ -8,15 +8,30 @@ import {
 import {
   Card, Input,
 } from 'react-native-elements';
+import { addDailyQuestionResponse } from '../../services/datastore';
 
 function CheckinSubmit({ navigation }) {
+  const [textAnswer, setTextAnswer] = useState('');
+
+  const handleOnSubmit = () => {
+    navigation.navigate('Checkin');
+    addDailyQuestionResponse(
+      {
+        pair_id: 'example',
+        question_id: 2,
+        response: textAnswer,
+        user_id: 'example',
+      },
+    );
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <Card containerStyle={styles.cardContainer}>
         <Text>Daily Question</Text>
         <Card.Title style={styles.question}>What is your most treasured memory of us?</Card.Title>
-        <Input placeholder="Type your response" multiline />
-        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Checkin')}>
+        <Input value={textAnswer} onChangeText={setTextAnswer} placeholder="Type your response" multiline />
+        <TouchableOpacity style={styles.button} onPress={handleOnSubmit}>
           <Text style={styles.buttonText}>
             Submit
           </Text>
