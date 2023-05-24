d8e7fc559624118514c88eba4a82f67278aa0ec9
Soo <parksk971031@gmail.com>
2023-05-24 17:03:30 -0400

    merge

--- project-meu/src/screens/checkin/CheckinPage.js
+++ project-meu/src/screens/checkin/CheckinPage.js
@@ -15,7 +15,7 @@ import { getDailyQuestionResponses } from '../../services/datastore';
 function CheckinPage({ navigation }) {
   const [fontLoaded, setFontLoaded] = useState(false);
   const handleGetDailyQuestionResponses = () => {
-    getDailyQuestionResponses('example');
+    getDailyQuestionResponsegits('example');
   };
 
   useEffect(() => {
