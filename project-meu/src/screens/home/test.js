588e9b6f089cfe45389f93440e9f58e194a43eff
Soo <parksk971031@gmail.com>
2023-05-25 02:05:11 -0400

    update0

--- project-meu/src/screens/home/test.js
+++ project-meu/src/screens/home/test.js
@@ -140,14 +140,13 @@ import React, { useState } from 'react';
 import { Alert, Modal, StyleSheet, Text, Pressable, View, TextInput } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import Button from '../../components/Button';
-import DatePicker from 'react-native-date-picker';
-// import RegistrationInput from '../../components/RegistrationInput';
+import { FAB } from 'react-native-elements';
+
+import RegistrationInput from '../../components/RegistrationInput';
 
 const Test = () => {
   const [modalVisible, setModalVisible] = useState(false);
-  // const [date, setDate] = useState('');
-  const [date, setDate] = useState(new Date());
-  const [showDatePicker, setShowDatePicker] = useState(false);
+  const [date, setDate] = useState('');
   const [title, setTitle] = useState('');
   const [repeat, setRepeat] = useState('');
 
@@ -184,64 +183,64 @@ const Test = () => {
           <View style={styles.modalView}>
             <View style={styles.modalContent}>
               <Text style={styles.title}>Date</Text>
-              {/* <View style={[styles.container2]}>
-                <View style={[styles.inputContainer]}>
-                  <TextInput style={styles.input} placeholder="{placeholder}" editable="true" />
-                </View>
-                <View style={styles.line} />
-              </View> */}
-              {/* <DatePicker date={date} onDateChange={setDate} /> */}
-              {/* <DatePicker
-        modal
-        open='true'
-        date={date}
-        onConfirm={(date) => {
-          setOpen(false)
-          setDate(date)
-        }}
-        onCancel={() => {
-          setOpen(false)
-        }}
-      /> */}
+              <TextInput
+                style={styles.input}
+                placeholder="Enter Date"
+                value={date}
+                onChangeText={setDate}
+              />
+
 
               <Text style={styles.title}>Title</Text>
-              <View style={[styles.container2]}>
-                <View style={[styles.inputContainer]}>
-                  <TextInput style={styles.input} placeholder="Enter Title" editable="true" />
-                </View>
-                <View style={styles.line} />
-              </View>
-
-              <Text style={styles.title}>Repeat</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="Enter Date"
+                value={date}
+                onChangeText={setDate}
+              />
+              <View style={styles.line} />
+              
               {/* <View style={[styles.container2]}>
-                <View style={[styles.inputContainer]}>
-                  <TextInput style={styles.input} placeholder="{placeholder}" editable="true" />
+                <View style={[styles.inputContainer2]}>
+                  <TextInput style={styles.input2} placeholder="Enter Title" editable="true" />
                 </View>
                 <View style={styles.line} />
               </View> */}
-              <Button title="Save Event" onPress={handleAddEvent} buttonStyle={{ top: 376 }} />
+
+              <Text style={styles.title}>Repeat</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="Enter Repeat"
+                value={repeat}
+                onChangeText={setRepeat}
+              />
             </View>
 
             <Pressable style={[styles.button, styles.buttonClose]} onPress={closeModal}>
               <Ionicons name="ios-close" size={24} color="black" />
             </Pressable>
 
-            
+            <Button title="Add Event" onPress={handleAddEvent} buttonStyle={{ top: 395 }} />
           </View>
         </View>
       </Modal>
 
-      <DatePicker
-  date={date}
-  onDateChange={setDate}
-  mode="date"
-/>
+      <FAB
+        icon={<Ionicons name="ios-add" size={24} color="white" />}
+        buttonStyle={styles.FAB}
+        size="large"
+        placement='right'
+        onPress={handlePress}
+    />
 
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+    FAB:{
+      backgroundColor:'rgba(230, 43, 133, 1)',
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
@@ -267,8 +266,6 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 4,
     elevation: 5,
-    width:342,
-    height:475,
   },
   modalContent: {
     alignItems: 'flex-start',
@@ -298,36 +295,17 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
   input: {
-    height: 40,
+    height: 56,
     width: 300,
     borderColor: 'gray',
-    borderWidth: 1,
-    marginBottom: 10,
-    paddingHorizontal: 10,
-  },
-  inputContainer: {
-    width: 300,
-    height: 56,
-    backgroundColor: 'rgba(0, 0, 0, 0)',
-    justifyContent: 'center',
-    paddingHorizontal: 0,
-    alignItems: 'left',
-  },
-  input: {
     fontSize: 16,
     color: '#828282',
-    textAlign: 'center',
   },
   line: {
     width: 300,
     height: 1,
     backgroundColor: '#4f4f4f',
     alignSelf: 'center',
-  },
-  container333: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
 });
 
