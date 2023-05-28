9e96bf3e70ec8171da6e8f29906fbd77f6a5b6bd
Soo <parksk971031@gmail.com>
2023-05-28 01:47:32 -0400

    ongoing

--- project-meu/src/screens/home/HomeCalendar.js
+++ project-meu/src/screens/home/HomeCalendar.js
@@ -56,6 +56,30 @@ function DdayList({
   );
 }
 
+// const printEventTitlesAndDates = () => {
+//   getEvents()
+//     .then((events) => {
+//       const ddayList = events.map((event) => {
+//         const { title, date } = event;
+//         return (
+//           <DdayList
+//             key={event.id} // 적절한 key 값을 제공해야 합니다.
+//             date={date}
+//             title={title}
+//             iconName="yourIconName" // 필요한 아이콘 이름을 제공해야 합니다.
+//           />
+//         );
+//       });
+
+//       // ddayList 변수에는 각 이벤트에 대한 DdayList 컴포넌트들이 저장되어 있습니다.
+//       // 이제 이 변수를 화면에 렌더링하거나 필요한 곳에서 사용할 수 있습니다.
+//       console.log('Dday List:', ddayList);
+//     })
+//     .catch((error) => {
+//       console.error('Error getting events:', error);
+//     });
+// };
+
 // Modified from: https://github.com/kosaikham/twitter-scrollable-header-clone
 function HomeCalendarComponent({ scrollY, navigation }) {
   const THRESHOLD = 480;
