04b06b4647050503bc93971cc9d00c0461fe8c42
kpomm <katherine.l.pommerening.25@dartmouth.edu>
2023-05-29 15:17:10 -0400

    Clock and Location Done, WIP Thumbnail

--- project-meu/src/screens/home/BackgroundChange.js
+++ project-meu/src/screens/home/BackgroundChange.js
@@ -6,11 +6,10 @@ import { Camera } from 'expo-camera';
 import * as ImagePicker from 'expo-image-picker';
 import TopBar from '../../components/TopBar';
 import ClockAndLocation from '../../components/ClockAndLocation';
+import PictureThumbnail from '../../components/PictureThumbnail';
 
-const { width, height } = Dimensions.get('window');
-
-function BackgroundChange() {
-  const [backgroundImage, setBackgroundImage] = useState(null);
+function BackgroundChange({ navigation }) {
+  const [backgroundImage, setBackgroundImage] = useState('https://www.figma.com/file/PYeh3GKvg4VwmsTEXIc0Bs/image/d8a98af1d41d8274cf130bbb5bf82d5862df78f6?fuid=1112504140237920766');
   const [isMenuVisible, setMenuVisible] = useState(false);
   const [backgroundColor, setBackgroundColor] = useState('transparent');
 
@@ -79,13 +78,15 @@ function BackgroundChange() {
           resizeMode="cover"
         />
       )}
-
+      <View>
+        <TopBar navigation={navigation} />
+        <PictureThumbnail />
+      </View>
       <View style={styles.container}>
 
         <TouchableOpacity style={styles.iconButton} onPress={toggleMenu}>
           <Text>Menu Icon</Text>
         </TouchableOpacity>
-
         <Modal
           visible={isMenuVisible}
           transparent
