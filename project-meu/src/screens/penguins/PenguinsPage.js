96b5a08533dd39b1db5ec9d22cae8fe4096a82bd
楊蕾 <esther@yangleis-MBP.lan>
2023-06-11 00:44:28 -0400

    added initiail expo push notification

--- project-meu/src/screens/penguins/PenguinsPage.js
+++ project-meu/src/screens/penguins/PenguinsPage.js
@@ -1,12 +1,74 @@
 /* eslint-disable global-require */
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import {
-  Text, Button, SafeAreaView, StyleSheet, View, Image, Dimensions, Modal,
+  Text, Button, SafeAreaView, StyleSheet, View, Image, Dimensions, Modal, Platform,
 } from 'react-native';
 import Carousel from 'react-native-snap-carousel';
 import axios from 'axios';
 import { getAuth } from 'firebase/auth';
 import { apiUrl } from '../../constants/constants';
+import * as Notifications from 'expo-notifications';
+// import expoPushTokensApi from './src/api/expoPushTokens';
+import * as Device from 'expo-device';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+ });
+
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log("Expo push token:", token);
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+ 
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+ 
+  return token;
+}
+ 
+async function sendPushNotification(expoPushToken) {
+   const message = {
+     to: expoPushToken,
+     sound: 'default',
+     title: 'Test title',
+     body: 'Test body 2',
+     data: { testData: 'test data' },
+   };
+  
+   await fetch('https://exp.host/--/api/v2/push/send', {
+     method: 'POST',
+     headers: {
+       Accept: 'application/json',
+       'Accept-encoding': 'gzip, deflate',
+       'Content-Type': 'application/json',
+     },
+     body: JSON.stringify(message),
+   });
+} 
 
 const iconData = [
   require('../../../assets/penguin_icons/neutral.png'),
@@ -60,6 +122,11 @@ function PenguinsPage() {
   const [partnerDoc, setPartnerDoc] = useState('');
   const [partnerName, setPartnerName] = useState('');
   const [partnerLastEmotion, setPartnerLastEmotion] = useState(null);
+
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
 
   const auth = getAuth();
 
@@ -119,14 +186,40 @@ function PenguinsPage() {
     setSelectedIcon(Number(userDoc.user_last_emotion));
   }, [userDoc]);
 
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+ 
+    // This listener is fired whenever a notification is received while the app is foregrounded
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+        console.log('--- notification received ---');
+        console.log(notification);
+        setNotification(notification);
+        console.log('------');
+    });
+ 
+    // This listener is fired whenever a user taps on or interacts with a notification
+    // (works when app is foregrounded, backgrounded, or killed)
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+        console.log('--- notification tapped ---');
+        console.log(response);
+        console.log('------');
+    });
+ 
+    // Unsubscribe from events
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
   const renderIconItem = ({ item, index }) => {
     const itemStyle = index === selectedIcon ? styles.selectedIcon : styles.unselectedIcon;
-    const marginLeft = (-screenWidth / 100) * 4.4;
+    // const marginLeft = (-screenWidth / 100) * 4.4;
 
     return (
       <Image
         source={item}
-        style={[styles.icon, itemStyle, { marginLeft }]}
+        style={[styles.icon, itemStyle]} //, { marginLeft }]}
       />
     );
   };
@@ -187,12 +280,12 @@ function PenguinsPage() {
         <Text style={[styles.penguinName, { flex: 1 }]}>{userName}</Text>
         <Text style={[styles.penguinName, { flex: 1 }]}>{partnerName}</Text>
       </View>
-      <View style={[styles.carouselContainer, { marginTop: screenHeight * 0.02, marginBottom: screenHeight * 0.0644 }]}>
+      <View style={[styles.carouselContainer, { marginTop: screenHeight * 0.02, marginBottom: screenHeight * 0.0644, marginLeft: (-screenWidth / 100) * 5.4 }]}>
         <View
           style={[styles.circle, {
             borderRadius: screenHeight * 0.1395,
             borderWidth: screenWidth * 0.0186,
-            transform: [{ translateX: -screenWidth * 0.0865 }, { translateY: -screenHeight * 0.041 }],
+            transform: [{ translateX: -screenWidth * 0.0610 }, { translateY: -screenHeight * 0.057 }],
           }]}
         />
 
@@ -212,7 +305,10 @@ function PenguinsPage() {
         <View style={[styles.buttonContainer, { marginTop: -screenHeight * 0.035 }]}>
           <Button
             title="Send Emotion"
-            onPress={handleButtonPress}
+            onPress={() => {
+              handleButtonPress();
+              sendPushNotification(expoPushToken);
+            }}
             color="white"
           />
         </View>
