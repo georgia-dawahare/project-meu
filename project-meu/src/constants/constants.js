444ac6e5e7c1dde7da95470f5052889bdfa1584e
Georgia <84247852+georgia-dawahare@users.noreply.github.com>
2023-06-18 11:48:45 -0400

    Update constants.js

--- project-meu/src/constants/constants.js
+++ project-meu/src/constants/constants.js
@@ -1,6 +1,6 @@
-// const apiUrl = 'http://localhost:9090';
+const apiUrl = 'http://localhost:9090';
 // const apiUrl = 'http://172.24.15.139';
-const apiUrl = 'https://meu-forever.onrender.com';
+// const apiUrl = 'https://meu-forever.onrender.com';
 
 // Todo: Finish later
 const colors = {
