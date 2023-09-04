af85c0d8872079babea1ed75e2db096e0027da16
Soo <parksk971031@gmail.com>
2023-09-03 22:48:52 -0400

    checkinEdit completed

--- project-meu/src/screens/checkin/CheckinEdit.js
+++ project-meu/src/screens/checkin/CheckinEdit.js
@@ -20,19 +20,13 @@ import { useSelector, useDispatch } from 'react-redux';
 import { fetchQuestions } from '../../actions/QuestionsActions';
 import { fetchUserById } from '../../actions/UserActions';
 import {
-  fetchResponse, fetchResponseByUserId, fetchResponseByPartnerId, updateResponse,
+  fetchResponse, fetchResponseByUserId, updateResponse,
 } from '../../actions/ResponseActions';
-import { updateResponseGroup, fetchResponseGroupByPairId } from '../../actions/ResponseGroupActions';
+import { fetchResponseGroupByPairId } from '../../actions/ResponseGroupActions';
 
 function CheckinEdit({ navigation }) {
-  // need to be fixed : edit answer => no create Response & update the response
   const [textAnswer, setTextAnswer] = useState('');
-  const [setHandleonSubmit, HandleonSumbit] = useState(false);
-  const [newResponse, setNewResponse] = useState(true);
   const [submit, setSubmit] = useState(false);
-  const [edit, setEdit] = useState(false);
-  const userResponseCheck = '';
-  const partnerResponseCheck = '';
 
   const dispatch = useDispatch();
 
@@ -40,81 +34,34 @@ function CheckinEdit({ navigation }) {
   const user = useSelector((state) => state.userState.userData);
   const currUserId = user._id;
   const currUserPairId = user.pairId;
-  // console.log('user :      ', user);
-
-  // // Partner Data
-  const partner = useSelector((state) => state.partnerState.partnerData);
-  const partnerId = partner._id;
-  // console.log('partnerId', partnerId);
 
   // questions Data
   const questions = useSelector((state) => state.questionsState.questionsData);
-  // const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
 
   // fetch ResponseGroupData
   const currUserResponseGroup = useSelector((state) => state.responseGroupState.allResponseGroups);
   let currQuestionId = '';
-  let currUserResponseGroupId = '';
-  let latestResponseId1 = '';
-  let latestResponseId2 = '';
   if (currUserResponseGroup.length > 0) {
     const sortedResponseGroup = Object.values(currUserResponseGroup).sort((a, b) => {
       return parseInt(b.questionId, 10) - parseInt(a.questionId, 10);
     });
-
     const latestResonseGroup = sortedResponseGroup[0];
     currQuestionId = latestResonseGroup.questionId;
-    currUserResponseGroupId = latestResonseGroup._id;
-    latestResponseId1 = latestResonseGroup.responseId1;
-    latestResponseId2 = latestResonseGroup.responseId2;
-    // console.log('latestResonseGroup', latestResonseGroup);
-    // console.log('currUserResponseGroupId', currUserResponseGroupId);
   }
 
   const currQuestion = questions.length > 0 ? questions[currQuestionId].question : null;
 
   // response Data
-  // get User's Response
   const currUserResponse = useSelector((state) => state.responseState.allResponses);
   let latestUserResponse = '';
   let currUserResponseId = '';
-  let currUserResponseCreatedAt = '';
   if (currUserResponse) {
     const sortedUserResponse = Object.values(currUserResponse).sort((a, b) => {
       return new Date(b.createdAt) - new Date(a.createdAt);
     });
     latestUserResponse = sortedUserResponse[0];
     if (latestUserResponse) {
-      // currUserResponseText = latestUserResponse.response;
       currUserResponseId = latestUserResponse._id;
-      currUserResponseCreatedAt = latestUserResponse.createdAt;
-    }
-
-    console.log('currUserResponseId222222', currUserResponseId);
-  }
-
-  // get partnerResponse
-  const partnerResponse = useSelector((state) => state.responseState.partnerResponse);
-  let latestPartnerResponse = '';
-  let sortedPartnerResponse = '';
-  let partnerResponseCreatedAt = '';
-  // const partnerResponseText = '';
-  // const partnerResponseId = '';
-  if (partnerResponse) {
-    sortedPartnerResponse = Object.values(partnerResponse).sort((a, b) => {
-      return new Date(b.createdAt) - new Date(a.createdAt);
-    });
-    latestPartnerResponse = sortedPartnerResponse[0];
-    // console.log('latestPartnerResponse', latestPartnerResponse);
-
-    if (latestPartnerResponse) {
-      // partnerResponseText = latestPartnerResponse.response;
-      // partnerResponseId = latestPartnerResponse._id;
-      partnerResponseCreatedAt = latestPartnerResponse.createdAt;
-
-      if (partnerResponseCheck) {
-        latestPartnerResponse = sortedPartnerResponse[0];
-      }
     }
   }
 
@@ -139,80 +86,12 @@ function CheckinEdit({ navigation }) {
     fetchRepsonseData();
   }, [currUserId]);
 
-  useEffect(() => {
-    async function fetchPartnerResponse() {
-      if (partnerId) {
-        await dispatch(fetchResponseByPartnerId(partnerId));
-      }
-    }
-    fetchPartnerResponse();
-  }, [partnerId]);
-
   console.log('currUserResponseId', currUserResponseId);
   const handleOnEdit = async () => {
-    // if (currUserResponseId) {
-    // await dispatch(updateResponse(currUserResponseId, {
-    //   response: textAnswer,
-    // }));
-    // }
-
-    await dispatch(updateResponse('64f53db6210ed4715f62b443', {
+    await dispatch(updateResponse(currUserResponseId, {
       response: textAnswer,
     }));
 
-    // if (!userResponseCheck && !partnerResponseCheck) {
-    //   await dispatch(createResponse(currUserId, {
-    //     response: textAnswer,
-    //     userId: currUserId,
-    //     responseGroupId: currUserPairId,
-    //   }));
-    // }
-
-    // else if (latestResponseId1 && !latestResponseId2) {
-    //   await dispatch(updateResponseGroup(LatestResponseId, {
-    //     responseId2: LatestResponseId,
-    //   }));
-    // }
-
-    // Update the response group with the new response
-    // if (latestResponseId1) {
-    //   const updatedResponseGroup = {
-    //     responseId1: latestResponseId1 || createResponse._id,
-    //     responseId2: latestResponseId1 ? createResponse._id : latestResponseId2,
-    //   };
-
-    //   await dispatch(updateResponseGroup(currUserResponseGroupId, updatedResponseGroup));
-    // }
-
-    // await dispatch(updateResponseGroup(currUserResponseGroupId, {
-    //   // need to be fixed
-    //   responseId2: '64cb119616340a8f7439781',
-    // }));
-
-    // if (latestResponseId1) {
-    //   await dispatch(updateResponseGroup(currUserResponseGroupId, {
-    //     // need to be fixed
-    //     responseId2: '64cb119616340a8f7439781',
-    //   }));
-    // } else if (!latestResponseId1) {
-    //   await dispatch(updateResponseGroup(currUserResponseGroupId, {
-    //     responseId1: LatestResponseId,
-    //   }));
-    // }
-
-    if (!newResponse) {
-      // update Response
-
-    }
-    // } else {
-    //   await updateResponse(
-    //     currUserId,
-    //     {
-    //       response: textAnswer,
-    //       user_id: currUserId,
-    //     },
-    //   );
-    // }
     setSubmit(true);
     setSubmit(false);
   };
@@ -225,17 +104,6 @@ function CheckinEdit({ navigation }) {
     }
     fetchResponseData();
   }, [currUserId]);
-
-  // useEffect(() => {
-  //   async function updateRG() {
-  //     if (currUserResponseId) {
-  //       await dispatch(updateResponseGroup(currUserResponseGroupId, {
-  //         responseId1: currUserResponseId,
-  //       }));
-  //     }
-  //   }
-  //   updateRG();
-  // }, [HandleonSumbit]);
 
   return (
 
